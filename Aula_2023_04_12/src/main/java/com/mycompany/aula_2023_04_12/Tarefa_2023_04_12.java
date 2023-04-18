f120f50c0c6c58afd64b03b2afe53a5aae028c87
DougzCD <110394134+DougzCD@users.noreply.github.com>
2023-04-18 10:58:29 -0300

    Add files via upload

--- Aula_2023_04_12/src/main/java/com/mycompany/aula_2023_04_12/Tarefa_2023_04_12.java
+++ Aula_2023_04_12/src/main/java/com/mycompany/aula_2023_04_12/Tarefa_2023_04_12.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.aula_2023_04_12;
+import java.util.Arrays;
 
 /**
  *
@@ -10,20 +11,84 @@ package com.mycompany.aula_2023_04_12;
  */
 public class Tarefa_2023_04_12 {
     
-    public static int ex_1 (boolean c1, boolean c2, String[] resp) 
+    public static String[] ex_6 (boolean c1, boolean c2) 
     {      
-        resp[0] = "A";
-        System.out.println("A");
-        if (c1) 
-            resp[1] = "B";
-        else
-            resp[1] = "X";
-        if (c2) 
-            resp[2] = "C";
-        else
-            resp[2] = "Y";
+        String resp[] = new String[4];
         
-        return resp[1];
+        resp[0] = "A";
+        System.out.println(resp[0]);
+        if (c1){
+            resp[1] = "B";
+            System.out.println(resp[1]);
+        }
+        else{
+            resp[1] = "X";
+            System.out.println(resp[1]);
+        }   
+        if (c2){
+            resp[2] = "C";
+            System.out.println(resp[2]);
+        }
+        else{
+            resp[2] = "Y";
+            System.out.println(resp[2]);
+        }
+        
+        resp[3] = "D";
+        
+        System.out.println(resp[3]);
+        
+        return resp;
+    }
+    
+    public static int ex_7 (int[] valor) 
+    {      
+        int i=1, soma=0, totalentrada=0, totalvalid = 0, media;
+        
+        do{
+            totalentrada++;
+            int min = Arrays.stream(valor).min().getAsInt();
+            int max = Arrays.stream(valor).max().getAsInt();
+            
+            if(valor[i]>=min && valor[i]<=max){
+                totalvalid++;
+                soma = soma + valor[i];  
+            }
+            i++;  
+        }while(valor[i]!=-999 && totalentrada<100);
+        if(totalvalid > 0){
+            media = soma/totalvalid;
+        }else{
+            media = -999;
+        }
+        return media;
+    }
+    
+    public static int ex_8 (boolean premium, boolean casquinha, int coberturas){
+        
+        int preco;
+        
+        if (premium){
+            preco = 20;
+        }
+        else{
+            preco = 15;
+        }   
+        if (casquinha){
+            preco = preco + 2;
+        }
+        else{
+            preco = preco + 1;
+        }
+        if (coberturas>1){
+            preco = preco + 2;
+        }else{
+            preco = preco + 1;
+        }
+       
+        
+        return preco;
+        
     }
     
 }
